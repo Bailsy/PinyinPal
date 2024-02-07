3bb6b2bad70715b5b5cd063a29d1968e973a1bd6
Bailey Crossan <bailey@pinyinpal.com>
2024-02-07 10:18:18 +0000

    not more disgusting web view

--- lib/models/player.dart
+++ lib/models/player.dart
@@ -1,14 +1,15 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:video_player/video_player.dart';
 
 class VimeoPlayer extends StatefulWidget {
   const VimeoPlayer({
-    super.key,
+    Key? key,
     required this.videoId,
     this.width,
     this.height,
-  });
+  }) : super(key: key);
 
   final String videoId;
   final double? width;
@@ -19,30 +20,52 @@ class VimeoPlayer extends StatefulWidget {
 }
 
 class _VimeoPlayerState extends State<VimeoPlayer> {
+  late VideoPlayerController _controller;
+  bool _isInitialized = false;
+
+  @override
   void initState() {
     super.initState();
+    _initializeVideo();
+  }
 
-    getMp4Link(widget.videoId);
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _initializeVideo() async {
+    final videoURL = await getMp4Link(widget.videoId);
+    _controller = VideoPlayerController.network(videoURL)
+      ..initialize().then((_) {
+        setState(() {
+          _isInitialized = true;
+          // Start playing the video automatically when initialized
+          _controller.play();
+        });
+      });
   }
 
   static Future<String> getMp4Link(String videoId) async {
     final response = await http
         .get(Uri.parse('https://player.vimeo.com/video/${videoId}/config'));
-    // Process the response and convert it to List<ExampleSentence>
-
-    // Placeholder: Handle the response and return a List<ExampleSentence>
     if (response.statusCode == 200) {
       var msg = json.decode(response.body);
       final String videoURL = msg["request"]["files"]["progressive"][0]["url"];
       return videoURL;
     } else {
-      // Handle error case
-      return Future.error('Error fetching example sentences');
+      throw Exception('Failed to load video URL');
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox();
+    return _isInitialized
+        ? AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(_controller),
+          )
+        : Center(child: CircularProgressIndicator());
   }
 }
